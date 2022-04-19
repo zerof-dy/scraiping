bfb396650cfa409f3e4afb9c5c5e1a35e87e9c87
zerof <93659442+zerof-dy@users.noreply.github.com>
2022-04-19 15:07:33 +0900

    index.html page

--- script.py
+++ script.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "hello world"
 
 
 if __name__=="__main__":
